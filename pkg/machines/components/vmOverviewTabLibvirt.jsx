3d819247cb878c06f66f2a62335454a994182b42
Garrett LeSage <garrett@lesage.us>
2020-04-07 13:42:10 +0200

    machines: Fix in-table headings

--- pkg/machines/components/vmOverviewTabLibvirt.jsx
+++ pkg/machines/components/vmOverviewTabLibvirt.jsx
@@ -20,7 +20,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cockpit from 'cockpit';
 import { OverlayTrigger, Tooltip } from "patternfly-react";
-import { Button } from "@patternfly/react-core";
+import {
+    Button,
+    Text,
+    TextVariants
+} from "@patternfly/react-core";
 
 import { VCPUModal } from './vcpuModal.jsx';
 import MemoryModal from './vm/memoryModal.jsx';
@@ -100,7 +104,7 @@ class VmOverviewTabLibvirt extends React.Component {
                     if (this._isMounted)
                         this.setState({ loaderElems });
                 })
-                .fail(ex => console.warn("getDomainCapabilities failed"));
+                .fail(() => console.warn("getDomainCapabilities failed"));
     }
 
     onAutostartChanged() {
@@ -261,8 +265,9 @@ class VmOverviewTabLibvirt extends React.Component {
             <>
                 <div className="overview-tab-grid">
                     <div className='ct-form'>
-                        <label className='control-label label-title'> {_("General")} </label>
-                        <span />
+                        <Text component={TextVariants.h4} className='ct-form-full'>
+                            {_("General")}
+                        </Text>
                         <label className='control-label' htmlFor={`${idPrefix}-memory-count`}>{_("Memory")}</label>
                         {memoryLink}
                         <label className='control-label' htmlFor={`${idPrefix}-vcpus-count`}>{_("vCPUs")}</label>
@@ -277,8 +282,9 @@ class VmOverviewTabLibvirt extends React.Component {
                         </>}
                     </div>
                     <div className="ct-form">
-                        <label className='control-label label-title'> {_("Hypervisor Details")} </label>
-                        <span />
+                        <Text component={TextVariants.h4} className='ct-form-full'>
+                            {_("Hypervisor Details")}
+                        </Text>
                         <label className='control-label' htmlFor={`${idPrefix}-emulated-machine`}>{_("Emulated Machine")}</label>
                         <div id={`${idPrefix}-emulated-machine`}>{vm.emulatedMachine}</div>
                         {firmwareLinkWrapper ? <label className='control-label' htmlFor={`${idPrefix}-firmware`}>{_("Firmware")}</label> : null}

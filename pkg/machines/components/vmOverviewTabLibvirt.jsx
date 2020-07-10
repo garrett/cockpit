28e5dcf2f0f8783b912e76974e6fe2a9c6f10735
Katerina Koukiou <kkoukiou@redhat.com>
2020-07-10 10:41:36 +0200

    machines: Reimplement the design of the main VMs list
    The expandable content of each row was moved to a seperate page.
    
    Based on the design of #14196
    Fixes #14196
    Fixes #14552
    Fixes #14443
    Closes #14346
    This addresses https://bugzilla.redhat.com/show_bug.cgi?id=1862406

--- pkg/machines/components/vmOverviewTabLibvirt.jsx
+++ pkg/machines/components/vmOverviewTabLibvirt.jsx
@@ -37,6 +37,7 @@ import { BootOrderModal } from './vm/bootOrderModal.jsx';
 import { FirmwareModal } from './vm/firmwareModal.jsx';
 import WarningInactive from './warningInactive.jsx';
 import { supportsUefiXml, labelForFirmwarePath } from './vm/helpers.js';
+import { StateIcon } from './vm/stateIcon.jsx';
 import LibvirtDBus, { getDomainCapabilities } from '../libvirt-dbus.js';
 import { getDomainCapLoader, getDomainCapMaxVCPU } from '../libvirt-common.js';
 
@@ -258,6 +259,8 @@ class VmOverviewTabLibvirt extends React.Component {
                         <Text component={TextVariants.h4} className='ct-form-full'>
                             {_("General")}
                         </Text>
+                        <label className='control-label' htmlFor={`${idPrefix}-state`}>{_("State")}</label>
+                        <StateIcon state={vm.state} valueId={`${idPrefix}-state`} showIcon />
                         <label className='control-label' htmlFor={`${idPrefix}-memory-count`}>{_("Memory")}</label>
                         {memoryLink}
                         <label className='control-label' htmlFor={`${idPrefix}-vcpus-count`}>{_("vCPUs")}</label>

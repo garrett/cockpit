13d5cd7dcb0bffdbc6aae7b406984a1efb5b165a
Garrett LeSage <garrett@redhat.com>
2025-04-01 12:10:49 +0200

    about: Use masking instead

--- files.js
+++ files.js
@@ -123,7 +123,7 @@ const info = {
         "shell/images/server-large.png",
         "shell/images/server-small.png",
         "shell/images/cockpit-icon.svg",
-        "shell/images/cockpit-icon-gray.svg",
+        "shell/images/cockpit-icon-dark.svg",
         "shell/images/bg-plain.jpg",
         "shell/index.html",
         "shell/shell.html",
